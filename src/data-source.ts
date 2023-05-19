f6176a4a454707029786f22e81d501d5c4032c10
virginia-zuror <virginiazuor@gmail.com>
2023-05-19 11:46:07 +0200

    conenction database-mongoose

---
+++ src/data-source.ts
@@ -0,0 +1,39 @@
+import { logger } from "./logger";
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+/* const mongoose = require("mongoose");
+
+const mongoDb = process.env.MONGO_URI;
+
+let connection;
+
+export const connect = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (connection) return connection;
+
+  try {
+    connection = await mongoose.connect(mongoDb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    connection.connection.on("error", (error) => {
+      console.error(`DB connection error: ${error.message}`);
+    });
+    connection.connection.on("disconnected", () => {
+      console.error(`DB disconnected, reconnecting...`);
+      connect();
+    });
+    logger.info(`Connect to DB: ${connection.connection.name}`);
+    
+
+  } catch (error) {
+    console.error(`Not connect to DB: ${error.message}`);
+  }
+  return connection;
+}; */
+
+/* module.exports = { connect }; */
+
--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,43 @@ import { root } from "./routes/root";
 import { isInteger } from "./routes/utils";
 import { logger } from "./logger";
 
+
 const app = express();
 
+
+const mongoose = require("mongoose");
+
+const mongoDb = process.env.MONGO_URI;
+
+let connection;
+
+const connect = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (connection) return connection;
+
+  try {
+    connection = await mongoose.connect(mongoDb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    connection.connection.on("error", (error) => {
+      logger.error(`DB connection error: ${error.message}`);
+    });
+    connection.connection.on("disconnected", () => {
+      logger.error(`DB disconnected, reconnecting...`);
+      connect();
+    });
+    logger.info(`Connected to DB: ${connection.connection.name}`);
+    
+
+  } catch (error) {
+    logger.error(`Not connect to DB: ${error.message}`);
+  }
+  return connection;
+};
+
+connect();
 
 function setupExpress(){
 
@@ -24,7 +59,7 @@ function setupExpress(){
 
 }
 
-function startServer(){
+/* function startServer(){ */
     
     let port:number;
     const portEnv = process.env.PORT,
@@ -44,11 +79,13 @@ function startServer(){
     
 
     app.listen(port, ()=>{
+        setupExpress();
+        
         logger.info(`HTTP REST API Server is now running at http://localhost:${port}`);
         
     })
 
-}
+/* } */
 
-setupExpress();
-startServer();
+
+
